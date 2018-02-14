bbf4aba6fa9d5342ee32489d83c1ce62efd72d49
Travis Pritchard <35708200+TravisPritchardODEQ@users.noreply.github.com>
2018-02-14 15:32:45 -0800

    Add files via upload

--- olddatasubreformat.R
+++ olddatasubreformat.R
@@ -1,13 +1,19 @@
 library(readxl)
 library(dplyr)
 
+
+#Directory ot find files
 path = "//deqlab1/Vol_Data/socoast/2013Submit/2013 Multiparameter Data Submit/"
 
 
+#vector of filenames in directory
 in_fnames <- list.files(path, full.names = TRUE)
+
+#Do not include files with "List of" in name
 datafiles <- in_fnames[!grepl('List of', in_fnames)]
 
 
+#formatting excel import 
 ctypes <-  c("guess",
              "date",
              "guess",
@@ -29,16 +35,23 @@ cnames <- c("site",
             "DO_sat",
             "comments")
 
+#Create blank auditinfo table
 auditinfo <- data.frame(matrix(ncol = 9, nrow = 0))
 colnames(auditinfo) <- cnames
 
+
+#Create empty list to use for later binding
 datalist = list()
 
 
 
+#Field audit into For loop
+#For each datafile, pull out audit info and put into table
+
 for (m in 1:length(datafiles)) {
   
   fname <- datafiles[m]
+  #read portion of excel file with audit info
   table <-
     read_excel(
       fname,
@@ -48,17 +61,17 @@ for (m in 1:length(datafiles)) {
     )
 
   
-  logger <-
-    read_excel(
-      datafiles[m],
-      sheet = "LIMS_LASAR LOAD SHEET",
-      range = "F3:F3",
-      col_names = c("logger")
-    )
+  #read section with Lasar#
+  lasar <-
+    read_excel( fname,
+               range = "C5:C5",
+               col_names = c("ID")
+               
+    ) 
   
   
   table <- table %>%
-    mutate(site = logger$logger) %>%
+    mutate(site = lasar$ID) %>%
     mutate(time = strftime(time, format="%H:%M:%S")) %>%
     filter(!is.na(date))
   
